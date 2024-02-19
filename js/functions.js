353b8bf36ad65758edbccdf2fafb30c1c560bcb1
svetlana <svetlana1korolkova@gmail.com>
2024-02-19 08:01:59 +0300

    Нужно больше функций

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,21 @@
-const isLengthLessMax = (string) => {
-  const nub = string.length;
-  return nub;
+const isLengthLessMax = function (string, length) {
+  const num = string.length <= Number(length) ? 'true' : 'false';
+  return num;
 };
 
+console.log(isLengthLessMax('fgdddd', 15));
 
-isLengthLessMax('65455');
+const isPalindrom = (string) => {
+
+  const word = string.toLowerCase().replaceAll(' ');
+
+  let reversedWord = '';
+  for (let i = 1; i <= word.length; i++) {
+    reversedWord += word[word.length - i];
+  }
+  const text = reversedWord === word ? 'Palindrom' : 'Not palindrom';
+
+  return text;
+};
+
+console.log(isPalindrom('Ala'));
